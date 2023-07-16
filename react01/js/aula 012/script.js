3e2565363855e98d91cd95fef29db8b2486cadc6
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-07-16 08:52:06 -0300

    :zap: working with loops

---
+++ react01/js/aula 012/script.js
@@ -0,0 +1,29 @@
+// while = enquanto
+
+var x = 0;
+
+while(x < 10){
+    document.write("</br> O valor de X : " + x);
+    x++;
+}
+
+
+document.write("</br>")
+
+
+// for = para
+
+
+for(a = 0; a < 10; a++){
+    document.write("</br> O valor de A : " + a);
+}
+
+document.write("</br>")
+
+
+// for = para
+
+
+for(a = 0; a < x; a++){
+    document.write("</br> O valor de A : " + a);
+}
