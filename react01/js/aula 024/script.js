da3f798619efaeeab66b1225fa6323af6a1639bd
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-07-24 08:37:01 -0300

    :zap: includes endswith startwith

---
+++ react01/js/aula 024/script.js
@@ -0,0 +1,19 @@
+
+
+// includes
+// endsWith
+// startsWith
+
+let nomes = ["Matheus", "Lucas", "Joao"];
+
+console.log(nomes.includes("Lucas"));
+
+// if(nomes.includes("Matheus")) {
+//     console.log("Mateus esta na lista!");
+// } else {
+//     console.log("Mateus NAO esta na lista!");
+// }
+
+let nome = "Matheus";
+console.log(nome.endsWith("s"));
+console.log(nome.startsWith("Mat"));
