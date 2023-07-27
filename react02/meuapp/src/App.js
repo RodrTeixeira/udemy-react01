fa545eb6ffc9c9bc76c1d8f3444faccfcd5a092f
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-07-27 08:24:22 -0300

    :zap: props and component I

--- react02/meuapp/src/App.js
+++ react02/meuapp/src/App.js
@@ -1,9 +1,25 @@
 
-export default function App(){
-  return(
-    <div>
-      <h1>Bem-vindo ao sistema!</h1>
-      <h2>@sujeitoprogramador :)</h2>
-    </div>
-  )
+import React from "react";
+
+// const Bemvindo = () => <h1>Bem-vindo!!!</h1>;
+
+const Bemvindo = (props) => {
+    return (
+      <div>
+        <h2>Bem-vindo {props.nome}</h2>
+        <h3>Tenho {props.idade} anos</h3>
+      </div>
+    );
 }
+
+function App() {
+    return(
+      <div>
+          Olá Mundo!
+          <Bemvindo nome="Lucas" idade="12"/>
+          <Bemvindo nome="Antonio" idade="33"/>
+      </div>
+    );
+}
+
+export default App;
