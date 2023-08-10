e0100ddac68198ec57d314c8984c7d2e6cbb8dfc
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-08-10 08:31:38 -0300

    :zap: useEffect

--- react02/meuapp/src/App.js
+++ react02/meuapp/src/App.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
- 
+import React, { useState, useEffect } from 'react';
+
 function App() {
- 
-  // Declarar uma nova variável de state, na qual chamaremos de "contador"
-  const [contador, setContador] = useState(0);
- 
-  return (
+
+    const [tarefas, setTarefas] = useState([]);
+
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+      const tarefasStorage = localStorage.getItem('tarefas');
+      if(tarefasStorage){
+        setTarefas(JSON.parse(tarefasStorage));
+      }
+    }, []);
+
+    useEffect(() => {
+      localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    }, [tarefas])
+
+    function handleAdd(){
+      setTarefas([...tarefas, input])
+      setInput('');
+    }
+
+  return(
     <div>
-      <p>Você clicou {contador} vezes</p>
-      <button onClick={() => setContador(contador + 1)}>
-          Aumentar
-      </button>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
+
+
+  
 export default App;
