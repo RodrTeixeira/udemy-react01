08e797e19cd8167a0520b16b3c58cb70434f1163
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-08-16 08:19:58 -0300

    :zap: working with routes I

--- react02/meuapp/src/App.js
+++ react02/meuapp/src/App.js
@@ -1,43 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import "./style.css";
-;
-////https://sujeitoprogramador.com/rn-api/?api=posts
+import React from 'react';
+import "./index.css"
 
 function App() {
-  const [nutri, setNutri] = useState([]);
-
-  useEffect(() => {
-    function loadApi(){
-      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
-      fetch(url)
-      .then((r) => r.json())
-      .then((json) => {
-        console.log(json);
-        setNutri(json);
-      })
-    }
-    loadApi();
-  }, [])
-
-  return(
-    <div className='container'>
-      <header>
-        <strong>React Nutri</strong>
-      </header>
-      {nutri.map((item) => {
-        return (
-          <article key={item.id} className="post">
-            <strong className='titulo'>{item.titulo}</strong>
-            <img src={item.capa} alt={item.titulo} className="capa" />
-            <p className='subtitulo'>
-              {item.subtitulo}
-            </p>
-            <a className='botao'>Acessar</a>
-          </article>
-        )
-      })}
+  return (
+    <div>
+      <h1>TESTE</h1>
     </div>
-  );
+  )
 }
 
 export default App;
