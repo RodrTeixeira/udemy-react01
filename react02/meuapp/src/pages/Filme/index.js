17bde76626038b964feb1dccd986858c06d144cc
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-08-27 08:55:20 -0300

    :zap: movie details I

--- react02/meuapp/src/pages/Filme/index.js
+++ react02/meuapp/src/pages/Filme/index.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
 function Filme() {
+    const { id } = useParams();
+    useEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: "da78d99ef4c7fdd90e5ade18e4bf1a7a",
+                    language: "pt-BR",
+                }
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch(() => {
+                console.log("FILME NAO ENCONTRADO!!!")
+            })
+        }
+        loadFilme();
+    },[])
     return(
         <div>
-            <h1>BEM-VINDO À PÁGINA DETALHES DOS FILMES!</h1>
+            <h1>ACESSANDO FILME {id}</h1>
         </div>
     )
 }
