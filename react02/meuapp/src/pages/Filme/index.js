3b2df1b839028f28806bd173f448cd3afee61016
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-08-28 08:24:33 -0300

    :zap: movie details II

--- react02/meuapp/src/pages/Filme/index.js
+++ react02/meuapp/src/pages/Filme/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./filme.css";
 
 import api from "../../services/api";
 
 function Filme() {
     const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function loadFilme(){
             await api.get(`/movie/${id}`,{
@@ -14,17 +17,35 @@ function Filme() {
                 }
             })
             .then((response) => {
-                console.log(response.data);
+                setFilme(response.data);
+                setLoading(false);
             })
             .catch(() => {
                 console.log("FILME NAO ENCONTRADO!!!")
             })
         }
         loadFilme();
+        return () => {
+            console.log("COMPONENTE FOI DESMONTADO!")
+        }
     },[])
+
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>ACESSANDO FILME {id}</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} /10</strong>
+
         </div>
     )
 }
