c4020c88ce5a9576570feb537bdf00ee7a205c3d
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-08-29 08:12:12 -0300

    :zap: movie details III

--- react02/meuapp/src/pages/Filme/index.js
+++ react02/meuapp/src/pages/Filme/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./filme.css";
+import "./filme-info.css";
 
 import api from "../../services/api";
 
@@ -45,7 +45,14 @@ function Filme() {
             <h3>Sinopse</h3>
             <span>{filme.overview}</span>
             <strong>Avaliação: {filme.vote_average} /10</strong>
-
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
