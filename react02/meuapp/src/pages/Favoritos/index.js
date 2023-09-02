10229d60ea101cae7e80203580219201f81c6fe7
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-09-02 08:17:32 -0300

    :zap: creating favorite pages I

---
+++ react02/meuapp/src/pages/Favoritos/index.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import "./favoritos.css";
+
+function Favoritos(){
+
+    const [filmes, setFilmes] = useState([]);
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista) || [])
+    }, []);
+
+    return(
+        <div className="meus-filmes">
+            <h1>Meus Filmes</h1>
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Favoritos;
