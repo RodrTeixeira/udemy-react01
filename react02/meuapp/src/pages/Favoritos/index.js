ddfdcee1547dde0a191a187798177a2589f56f18
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-09-04 08:10:32 -0300

    :zap: manipulating movie list

--- react02/meuapp/src/pages/Favoritos/index.js
+++ react02/meuapp/src/pages/Favoritos/index.js
@@ -10,6 +10,14 @@ function Favoritos(){
         setFilmes(JSON.parse(minhaLista) || [])
     }, []);
 
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+        setFilmes(filtroFilmes);
+        localStorage.setItem("@primeflix", )
+    }
+
     return(
         <div className="meus-filmes">
             <h1>Meus Filmes</h1>
@@ -20,7 +28,7 @@ function Favoritos(){
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id) }>Excluir</button>
                             </div>
                         </li>
                     )
