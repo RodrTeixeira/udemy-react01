32ddeb0e05418b11a28ee2830e0ae3a81343d3f9
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-10-21 08:32:57 -0300

    :zap: creating register page I

--- react03/meuapp/src/pages/Home/index.js
+++ react03/meuapp/src/pages/Home/index.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Home(){
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    function handleLogin(e){
+    async function handleLogin(e){
       e.preventDefault();
       if(email !== "" && password !== ""){
-        alert("TESTE")
+        await signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          // navegar para /admin
+        })
+        .catch(() => {
+          console.log("ERRO AO FAZER O LOGIN")
+        })
       }else {
         alert("Preencha todos os campos!!!")
       }
--- react03/meuapp/src/pages/Register/index.js
+++ react03/meuapp/src/pages/Register/index.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Register(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleRegister(e){
+      e.preventDefault();
+      if(email !== "" && password !== ""){
+        alert("TESTE")
+      }else {
+        alert("Preencha todos os campos!!!")
+      }
+    }
+
     return(
-      <div>
-        <h1>Pagina REGISTER
+      <div className="home-container">
+        <h1> Cadastre-se
         </h1>
+        <span>Vamos criar sua conta</span>
+        <form className="form" onSubmit={handleRegister}>
+          <input 
+            type="text"
+            placeholder="Digite seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input 
+            autoComplete={false}
+            type="password"
+            placeholder="Digite a senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link className="button-link" to="/">
+          Já possui uma conta? Faça o login!
+        </Link>
       </div>
     )
   }
