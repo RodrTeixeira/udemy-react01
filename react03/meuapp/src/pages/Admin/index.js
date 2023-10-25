4f7505d43e10e3d3151314f98d9db0f33903d3da
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-10-25 08:21:42 -0300

    :zap: creating admin page I

--- react03/meuapp/src/pages/Admin/index.js
+++ react03/meuapp/src/pages/Admin/index.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./admin.css";
 
 export default function Admin() {
+    const [tarefaInput, setTarefaInput] = useState("")
+    function handleRegister(e){
+        e.preventDefault();
+        alert("CLICOU")
+    }
     return(
-        <div>
-            <h1>Página ADMIN!</h1>
+        <div className="admin-container">
+            <h1>Minhas Tarefas</h1>
+            <form className="form" onSubmit={handleRegister}>
+                <textarea
+                placeholder="Digite sua Tarefa..."
+                value={tarefaInput}
+                onChange={(e) => setTarefaInput(e.target.value)}
+                />
+                <button className="btn-register" type="submit">Registrar Tarefa</button>
+            </form>
+            <article className="list">
+                <p>Estudar JavaScript e ReactJS hoje a noite</p>
+                <div>
+                    <button>Editar</button>
+                    <button className="btn-delete">Concluir</button>
+                </div>
+            </article>
+            <button className="btn-logout">Sair</button>
         </div>
     )
 }
