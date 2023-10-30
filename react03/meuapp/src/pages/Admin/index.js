f4334f7df563e18e452d4078387c05d3f2a8b422
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-10-30 08:24:35 -0300

    :zap: searching for data in bank I

--- react03/meuapp/src/pages/Admin/index.js
+++ react03/meuapp/src/pages/Admin/index.js
@@ -4,17 +4,40 @@ import { auth,db } from "../../firebaseConnection";
 import { signOut } from "firebase/auth";
 import {
     addDoc,
-    collection
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    where
 } from "firebase/firestore";
 
 export default function Admin() {
     const [tarefaInput, setTarefaInput] = useState("")
     const [user, setUser] = useState({})
+    const [tarefas, setTarefas] = useState([]);
 
     useEffect(() => {
         async function loadTarefas(){
             const userDetail = localStorage.getItem("@detailUser")
             setUser(JSON.parse(userDetail))
+            if(userDetail){
+                const data = JSON.parse(userDetail)
+                const tarefaRef = collection(db, "tarefas")
+                const q = query(tarefaRef, orderBy("created", "desc"),where("userUid", "==", data?.uid))
+                const unsub = onSnapshot(q, (snapshot) => {
+                    
+                    let lista = [];
+
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id:doc.id,
+                            tarefa:doc.data().tarefa,
+                            userUid: doc.data().userUid,
+
+                        })
+                    })
+                })
+            }
         }
         loadTarefas();
     }, [])
