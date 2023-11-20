76b8a6193c59f66b609be323c4203f7b3f4c7cec
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-11-20 08:16:01 -0300

    :zap: configuring firebase

---
+++ react05/meuapp/src/services/firebaseConnection.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAs1EVAIKlVpqoD-o-pU6uLjtN8-a6YzYo",
+    authDomain: "tickets-cc74f.firebaseapp.com",
+    projectId: "tickets-cc74f",
+    storageBucket: "tickets-cc74f.appspot.com",
+    messagingSenderId: "854226426892",
+    appId: "1:854226426892:web:3fcc76002f8263764a6229",
+    measurementId: "G-NQS6J6BGWR"
+  };
