c4b0c4053a9281b32502f10da6617e8680a7f531
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-11-21 08:25:25 -0300

    :zap: configuring firebase I

--- react05/meuapp/src/services/firebaseConnection.js
+++ react05/meuapp/src/services/firebaseConnection.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAs1EVAIKlVpqoD-o-pU6uLjtN8-a6YzYo",
     authDomain: "tickets-cc74f.firebaseapp.com",
@@ -7,3 +12,10 @@ const firebaseConfig = {
     appId: "1:854226426892:web:3fcc76002f8263764a6229",
     measurementId: "G-NQS6J6BGWR"
   };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const auth = getAuth(firebaseApp);
+  const db = getFirestore(firebaseApp);
+  const storage = getStorage(firebaseApp);
+
+  export {auth, db, storage};
