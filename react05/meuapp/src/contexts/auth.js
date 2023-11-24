abd6bd1fb1e832945061b1e057b0ac4334491182
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-11-24 08:10:18 -0300

    :zap: creating context api II

--- react05/meuapp/src/contexts/auth.js
+++ react05/meuapp/src/contexts/auth.js
@@ -1,14 +1,22 @@
 import { useState, createContext, useEffect} from "react";
 
-const AuthContext = createContext({});
+export const AuthContext = createContext({});
 
 function AuthProvider( {children } ){
     const [user, setUser] = useState(null)
+
+    function signIn(email, password){
+        console.log(email)
+        console.log(password);
+        alert("LOGADO COM SUCESSO!")
+    }
+
     return(
         <AuthContext.Provider 
             value={{
                 signed: !!user, //false
                 user,
+                signIn
             }}
         >
             {children}
