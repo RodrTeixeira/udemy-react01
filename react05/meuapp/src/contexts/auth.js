683da8df74a9987a4c13d29adca137f8a5b7c9aa
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-12-01 08:24:52 -0300

    :zap: logging user

--- react05/meuapp/src/contexts/auth.js
+++ react05/meuapp/src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect} from "react";
 import { auth, db } from "../services/firebaseConnection";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc, setDoc} from "firebase/firestore";
 
 import { toast } from "react-toastify";
@@ -15,10 +15,30 @@ function AuthProvider( {children } ){
 
     const navigate = useNavigate();
 
-    function signIn(email, password){
-        console.log(email)
-        console.log(password);
-        alert("LOGADO COM SUCESSO!")
+    async function signIn(email, password){
+        setLoadingAuth(true);
+
+        await signInWithEmailAndPassword(auth, email, password)
+        .then( async (value) => {
+            let uid = value.user.uid;
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef)
+            let data = {
+                uid: uid,
+                nome: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+            toast.success("Bem-vindo de volta!");
+            navigate("/dashboard");
+
+        })
+
+
     }
 
     async function signUp(email, password, name){
