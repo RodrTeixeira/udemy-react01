bad4b963930352cfc9fa2901f640a6f94528f785
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-12-06 07:58:30 -0300

    :zap: creating private pages I

--- react05/meuapp/src/contexts/auth.js
+++ react05/meuapp/src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect} from "react";
 import { auth, db } from "../services/firebaseConnection";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getDoc, setDoc} from "firebase/firestore";
 
 import { toast } from "react-toastify";
@@ -15,6 +15,19 @@ function AuthProvider( {children } ){
     const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        async function loadUser(){
+            const storageUser = localStorage.getItem("@ticketsPRO")
+
+            if (storageUser){
+                setUser(JSON.parse(storageUser))
+                setLoading(false);
+            }    
+            setLoading(false);        
+        }
+        loadUser();
+    }, [])
 
     async function signIn(email, password){
         setLoadingAuth(true);
@@ -79,6 +92,12 @@ function AuthProvider( {children } ){
         localStorage.setItem("@ticketsPRO", JSON.stringify(data))
     }
 
+    async function logout(){
+        await signOut();
+        localStorage.removeItem("@ticketsPRO");
+        setUser(null);
+    }
+
     return(
         <AuthContext.Provider 
             value={{
@@ -86,6 +105,7 @@ function AuthProvider( {children } ){
                 user,
                 signIn,
                 signUp,
+                logout,
                 loadingAuth,
                 loading
             }}
