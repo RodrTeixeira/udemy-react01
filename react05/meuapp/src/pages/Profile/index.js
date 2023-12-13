d1d31066e7f30c81a4a0865027ab80997d1d93d2
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-12-13 08:17:22 -0300

    :zap: title component

---
+++ react05/meuapp/src/pages/Profile/index.js
@@ -0,0 +1,15 @@
+
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+
+export default function Profile(){
+    return(
+        <div>
+            <Header/>
+            <div>
+                <Title/>
+            </div>
+            <h1>Página de Perfil</h1>
+        </div>
+    )    
+}
