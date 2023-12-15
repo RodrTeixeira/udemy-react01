fb842bd2db16409e14e66db6ec5abee8423c79f4
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-12-15 08:06:29 -0300

    :zap: creating profile

--- react05/meuapp/src/pages/Profile/index.js
+++ react05/meuapp/src/pages/Profile/index.js
@@ -1,10 +1,17 @@
 
+import { useContext, useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 
-import { FiSettings } from "react-icons/fi";
+import { FiSettings, FiUpload } from "react-icons/fi";
+import avatar from "../../assets/avatar.png";
+import { AuthContext } from "../../contexts/auth";
 
 export default function Profile(){
+    const { user } = useContext(AuthContext);
+
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+
     return(
         <div>
             <Header/>
@@ -12,8 +19,22 @@ export default function Profile(){
                 <Title name="Minha Conta">
                     <FiSettings size={25}/>
                 </Title>
+                <div className="container">
+                    <form className="form-profile">
+                        <label className="label-avatar">
+                            <label>
+                                <FiUpload color="#fff" size={25} />
+                            </label>
+                            <input type="file" accept="image/*"/> <br/>
+                            {avatarUrl === null ? (
+                                <img src={avatar} alt="Foto de Perfil" width={250} height={250} />
+                            ) : (
+                                <img src={avatarUrl} alt="Foto de Perfil" width={250} height={250} />
+                            )}
+                        </label>
+                    </form>
+                </div>
             </div>
-            <h1>Página Perfil</h1>
         </div>
     )    
 }
