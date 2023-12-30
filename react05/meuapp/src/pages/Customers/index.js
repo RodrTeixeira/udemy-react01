0e595936f4fbd05a606bace2b6ff994caa082306
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-12-30 08:14:01 -0300

    :zap: customers page

---
+++ react05/meuapp/src/pages/Customers/index.js
@@ -0,0 +1,17 @@
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+
+import { FiUser } from "react-icons/fi";
+
+export default function Customers(){
+    return(
+        <div>
+            <Header/>
+            <div className="content">
+                <Title name="Clientes">
+                    <FiUser size={25}/>
+                </Title>
+            </div>
+        </div>
+    )
+}
