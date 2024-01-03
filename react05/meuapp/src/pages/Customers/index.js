979f8fe9e0a36f0a7c7e3b455ce944f62e6161ad
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-01-03 08:19:37 -0300

    :zap: customers page II

--- react05/meuapp/src/pages/Customers/index.js
+++ react05/meuapp/src/pages/Customers/index.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 
 import { FiUser } from "react-icons/fi";
 
 export default function Customers(){
+    const [nome, setNome]  = useState("")
+    const [cnpj, setCnpj]  = useState("")
+    const [endereco, setEndereco]  = useState("")
+
+    function handleRegister(e){
+        e.preventDefault();
+        alert("TESTE")
+    }
     return(
         <div>
             <Header/>
@@ -13,9 +22,31 @@ export default function Customers(){
                 </Title>
 
                 <div className="container">
-                    <form className="form-profile">
+                    <form className="form-profile" onSubmit={handleRegister}>
                         <label>Nome Fantasia</label>
-                        <input/>
+                        <input
+                            type="text"
+                            placeholder="Nome da Empresa"
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                        />
+                        <label>CNPJ</label>
+                        <input
+                            type="text"
+                            placeholder="Digite o CNPJ"
+                            value={cnpj}
+                            onChange={(e) => setCnpj(e.target.value)}
+                        />
+                        <label>Endereco</label>
+                        <input
+                            type="text"
+                            placeholder="Endereco da Empresa"
+                            value={endereco}
+                            onChange={(e) => setEndereco(e.target.value)}
+                        />
+                        <button type="submit">
+                            Salvar
+                        </button>
                     </form>
                 </div>
             </div>
