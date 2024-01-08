f62fb4320bdbf7d67251fe9a428a60deb7d1a1f6
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-01-08 08:28:20 -0300

    :zap: registering customers I

--- react05/meuapp/src/pages/Dashboard/index.js
+++ react05/meuapp/src/pages/Dashboard/index.js
@@ -3,6 +3,12 @@ import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header";
 
+import Title from "../../components/Title";
+import { FiPlus, FiMessageSquare } from "react-icons/fi";
+
+import "./dashboard.css";
+import { Link } from "react-router-dom";
+
 export default function Dashboard(){
     const { logout } = useContext(AuthContext);
 
@@ -13,8 +19,19 @@ export default function Dashboard(){
     return(
         <div>
             <Header/>
-            <h1>Página Dashboard</h1>
-            <button onClick={handleLogout}>Sair da Conta</button>
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+                <>
+                    <Link>
+                        <FiPlus color="#fff" size={25} />
+                    </Link>
+                    <div className="contaoiner">
+                        <h1>Teste</h1>
+                    </div>
+                </>
+            </div>
         </div>
     )
 }
