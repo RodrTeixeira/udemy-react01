b1e7165dbbdd1236f51b0706cf1f36604bbc0728
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-01-18 08:15:00 -0300

    :zap: new page called I

--- react05/meuapp/src/pages/New/index.js
+++ react05/meuapp/src/pages/New/index.js
@@ -20,6 +20,41 @@ export default function New(){
                             <option key={1} value={1}>Mercado Teste</option>
                             <option key={2} value={2}>Loja Informática</option>
                         </select>
+                        <label>Assunto</label>
+                        <select>
+                            <option value="Suporte">Suporte</option>
+                            <option value="Visita Técnica">Visita Técnica</option>
+                            <option value="Financeiro">Financeiro</option>
+                        </select>
+                        <label>Status</label>
+                        <div className="status">
+                            <input
+                                type="radio"
+                                name="radio"
+                                Value="Aberto"
+                            />
+                            <span>Em Aberto</span>
+
+                            <input
+                                type="radio"
+                                name="radio"
+                                Value="Progresso"
+                            />
+                            <span>Progresso</span>
+
+                            <input
+                                type="radio"
+                                name="radio"
+                                Value="Atendido"
+                            />
+                            <span>Atendido</span>
+                        </div>
+                        <label>Complemento</label>
+                        <textarea
+                            type="text"
+                            placeholder="Descreva seu problema (opcional)"
+                        />
+                        <button type="submit">Registrar</button>
                     </form>
                 </div>
             </div>
