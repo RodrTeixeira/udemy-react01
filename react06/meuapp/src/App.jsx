9f689fbb2d9bc400f60825990a0c15dfe9f624ad
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-04-11 08:17:12 -0300

    :zap: applying react hook form IV

--- react06/meuapp/src/App.jsx
+++ react06/meuapp/src/App.jsx
@@ -6,9 +6,10 @@ import { useForm } from "react-hook-form";
 
 function App() {
 
-  function handleSave(e){
-    e.preventDefault();
+  const { register, handleSubmit } = useForm()
 
+  function handleSave(data){
+    console.log(data);
   }
 
 
@@ -17,24 +18,30 @@ function App() {
       <h1>React</h1>
       <Header/>
 
-      <form className="form" onSubmit={handleSave}>
+      <form className="form" onSubmit={handleSubmit(handleSave)}>
         
         <input
           type="text"
           placeholder="Digite seu nome..."
           className="input"
+          {...register("name")}
+          id="name"
         />
     
         <input
           type="text"
           placeholder="Digite seu email..."
           className="input"
+          {...register("email")}
+          id="email"
         />
 
         <input
           type="text"
           placeholder="Digite seu username..."
           className="input"
+          {...register("username")}
+          id="username"
         />
 
         <button className="button" type="submit">Enviar</button>
