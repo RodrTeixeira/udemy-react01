d3c2f6abdffde10b9d1186edce36e7a7c3c1a076
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-04-27 09:11:15 -0300

    :zap: Create memorized components II

--- react06/meuapp/src/App.jsx
+++ react06/meuapp/src/App.jsx
@@ -5,9 +5,16 @@ import { Header } from './Header';
 
 function App() {
 
+  const [name, setNmae] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div className="container">
       <Header/>
+
+    <input
+    
+    />
 
     </div>
   )
