add28c55dc76af68127f9b435f96a0b89170c112
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-04-28 08:27:02 -0300

    :zap: Create memorized components III

--- react06/meuapp/src/App.jsx
+++ react06/meuapp/src/App.jsx
@@ -5,16 +5,27 @@ import { Header } from './Header';
 
 function App() {
 
-  const [name, setNmae] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
   return (
     <div className="container">
       <Header/>
 
+    <p>Nome:</p>
     <input
-    
+      placeholder="Digite seu nome..."
+      value={name}
+      onChange={(e) => setName(e.target.value) }
     />
+
+    <p>Email:</p>
+    <input
+      placeholder="Digite seu Email..."
+      value={email}
+      onChange={(e) => setEmail(e.target.value) }
+    />
+    <br/>
 
     </div>
   )
