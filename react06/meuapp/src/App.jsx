1c5cef3858ea8b2f4fa2924abde1056e6ac83c83
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-05-04 08:47:54 -0300

    :zap: Create memorized components VIII

--- react06/meuapp/src/App.jsx
+++ react06/meuapp/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import './App.css'
-import { MemorizedHeader } from './Header';
+import { Header } from './Header';
 
 function App() {
 
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="container">
-      <MemorizedHeader name={name} />
+      <Header name={name} />
 
     <p>Nome:</p>
     <input
