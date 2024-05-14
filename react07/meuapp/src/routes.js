cbc183cfc4fbe5a0bf4419b6216d23ef34450773
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-05-14 08:01:18 -0300

    :zap: Starting project VI

--- react07/meuapp/src/routes.js
+++ react07/meuapp/src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import Main from "./pages/Main";
 import Repositorio from "./pages/Repositorio";
@@ -7,10 +8,10 @@ import Repositorio from "./pages/Repositorio";
 export default function Routes(){
     return(
         <BrowserRouter>
-            <Switch>
+           <Switch>
                 <Route exact path="/" component={Main}/>
-                <Route exact path="/repositorio" component={Repositorio}/>
-            </Switch>
+                <Route exact path="/repositorio/:repositorio" component={Repositorio}/>
+            </Switch>        
         </BrowserRouter>
     );
 
