47edc4ce0639be374c25caafeaf3f2391e276926
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-07-17 07:31:02 -0300

    :zap: styling main page III

--- react07/meuapp/src/pages/Main/styles.js
+++ react07/meuapp/src/pages/Main/styles.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-   
+   max-width: 700px;
+   background: #FFF;
+   border-radius: 4px;
+   box-shadow: 0 0  20px rgba(0,0,0,0.2);
+   padding: 30px;
+   margin: 80px auto;
+
+
 `;
 
 export const Form = styled.form`
