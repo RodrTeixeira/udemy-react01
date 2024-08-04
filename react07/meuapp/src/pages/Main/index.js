8dfe4af242feb959d8e6e7281271f35946839663
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-08-04 16:19:08 -0300

    :zap: searching for repos XI

--- react07/meuapp/src/pages/Main/index.js
+++ react07/meuapp/src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import {FaGithub, FaPlus} from "react-icons/fa";
 
 import {Container, Form, SubmitButton} from "./styles";
