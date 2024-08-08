1349fe082f7cef551e8aa627db139dce2930e166
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-08-08 07:31:54 -0300

    :zap: searching for repos XV

--- react07/meuapp/src/pages/Main/index.js
+++ react07/meuapp/src/pages/Main/index.js
@@ -11,7 +11,9 @@ export default function Main(){
     const [repositorios, setRepositorios] = useState([]);
 
     const handleSubmit = useCallback((e)=>{
-        
+        async function submit(){
+            
+        }
     },[])
        
 
