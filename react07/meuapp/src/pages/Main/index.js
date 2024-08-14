a8a4a0edf3d3e67ba53bf6f65e1ffd6065c5502e
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-08-14 07:42:47 -0300

    :zap: seeking repos pt 2 II

--- react07/meuapp/src/pages/Main/index.js
+++ react07/meuapp/src/pages/Main/index.js
@@ -25,6 +25,8 @@ export default function Main(){
                 }
                 setRepositorios([...repositorios,data]);
                 setNewRepo('');
+            }catch(error){
+                
             }
 
         }
