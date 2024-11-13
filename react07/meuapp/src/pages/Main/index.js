05127df35574f6245a304af29981f89401a01a07
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-11-13 07:22:42 -0300

    :zap: Listing Repositories XXXXXXI

--- react07/meuapp/src/pages/Main/index.js
+++ react07/meuapp/src/pages/Main/index.js
@@ -46,7 +46,7 @@ export default function Main(){
     const handleDelete = useCallback((repo)=> {
         const find = repositorios.filter(r => r.name !== repo)
         setRepositorios(find);
-    }, [])
+    }, [repositorios])
 
     return(
         <Container>
