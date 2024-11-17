c29a39009e36e743ae3d263b5d889153e426853c
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2024-11-17 09:58:10 -0300

    :zap: Saving Repos III

--- react07/meuapp/src/pages/Main/index.js
+++ react07/meuapp/src/pages/Main/index.js
@@ -21,7 +21,7 @@ export default function Main(){
 
             try{
 
-             if(newRepo ==)   
+             if(newRepo === '')   
 
                 const response = await api.get(`repos/${newRepo}`);
 
